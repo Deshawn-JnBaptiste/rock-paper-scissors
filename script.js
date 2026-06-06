69de4919c5a722f79bbeebf1f06873fa24199e03
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-06-05 22:00:37 -0400

    Create a function called playGame and move playRound into it, call the playerRound function 5 times using a for loop

--- script.js
+++ script.js
@@ -24,49 +24,56 @@ function getHumanChoice() {
 
 }
 
-function playRound (humanChoice, computerChoice) {
-    //Player chooses Rock
-    if (humanChoice == "rock" && computerChoice == "rock") {
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound (humanChoice, computerChoice) {
+        //Player chooses Rock
+        if (humanChoice == "rock" && computerChoice == "rock") {
         console.log("You tie, You both chose rock.")
-    }
-    else if (humanChoice == "rock" && computerChoice == "paper") {
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lost, the computer chose paper")
         computerScore++;
-    }
-    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You won, the computer chose scissors")
         humanScore++;
-    }
+        }
 
-    //Player chooses Paper
-    else if (humanChoice == "paper" && computerChoice == "paper") {
+        //Player chooses Paper
+        else if (humanChoice == "paper" && computerChoice == "paper") {
         console.log("You tie, You both chose paper.")
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lost, the computer chose scissors")
         computerScore++;
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You won, the computer chose rock")
         humanScore++;
-    }
+        }
 
-    //Player chooses Scissors
-    else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        //Player chooses Scissors
+        else if (humanChoice == "scissors" && computerChoice == "scissors") {
         console.log("You tie, You both chose scissors.")
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lost, the computer chose rock")
         computerScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You won, the computer chose paper")
         humanScore++;
+        }
+    }
+    //Call the playRound function 5 times
+    for (let i = 0; i < 5; i++) {
+     playRound(getHumanChoice(), getComputerChoice())
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+playGame()
 
-getComputerChoice()
-getHumanChoice()
+
